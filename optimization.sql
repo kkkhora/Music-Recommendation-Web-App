bf8387ada9975a06025c20aa33235ac207e26502
kkkhora <76552190+kkkhora@users.noreply.github.com>
2022-04-12 00:42:31 +0800

    Create optimization.sql

---
+++ optimization.sql
@@ -0,0 +1,11 @@
+SELECT Artist_name
+FROM Artist_info_dedup
+WHERE Artist_country = 'JP';
+
+CREATE  index country_index ON Artist_info_dedup(Artist_country);
+
+SELECT Artist_name
+FROM Artist_info_dedup
+WHERE Artist_country = 'JP';
+
+DROP INDEX country_index ON Artist_info_dedup(Artist_country);

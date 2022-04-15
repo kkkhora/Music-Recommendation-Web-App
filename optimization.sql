d56fdf3e2ce247be4c05bb3561c31cb1a6c5d40e
kkkhora <76552190+kkkhora@users.noreply.github.com>
2022-04-15 13:15:49 +0800

    Update optimization.sql

--- optimization.sql
+++ optimization.sql
@@ -12,7 +12,7 @@ DROP INDEX country_index ON Artist_info_dedup;
 
 
 CREATE VIEW Display_results AS
-    SELECT Song_info.Song_ID, Song_name, Artist_name, Album_year, Song_genre, Track_image
+    SELECT Song_info.Song_ID, Song_name, Mt.Artist_id, Artist_name, Album_year, Song_genre, Track_image
     FROM Song_info JOIN Master_table Mt on Song_info.Song_ID = Mt.Song_ID
         JOIN Album_info_dedup Aid on Mt.Album_id = Aid.Album_id
         JOIN Artist_info_dedup A on Mt.Artist_id = A.Artist_id

09a802797088db06639f6e912025c1b632d56e9e
ghaoning <57677343+ghaoning@users.noreply.github.com>
2022-04-20 17:49:48 +0800

    Update signin.js

--- Spotify Music Collection/pages/signin.js
+++ Spotify Music Collection/pages/signin.js
@@ -23,7 +23,7 @@ const Policy = () => {
                     </div>
                     <div className="col-lg-7">
                         <div className="account-img">
-                            <img src="https://images.unsplash.com/photo-1525081905268-fc0b46e9d786?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="shape-image" />
+                            <img src="https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="shape-image" />
                         </div>
                     </div>
                 </div>

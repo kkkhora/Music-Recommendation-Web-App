2b7afc649544c136c45b8d7f828e7ebdb762867c
fondatsang <97462055+fondatsang@users.noreply.github.com>
2022-04-26 00:35:41 +0800

    index page

--- Spotify Music Collection (Client)/components/Banner.js
+++ Spotify Music Collection (Client)/components/Banner.js
@@ -9,14 +9,14 @@ const Banner = () => {
 
     return(
         <div>
-        <section className="banner-section style-1"  style={{backgroundImage:"url('assets/images/banner/bg.jpg')"}}>
+        <section className="banner-section style-1"  style={{backgroundImage:"url('assets/images/banner/bg1.jpg')"}}>
             <div className="container">
                 <div className="banner-wrapper">
                     <div className="row align-items-center justify-content-center">
                         <div className="col-lg-8">
                             <div className="banner-content text-center">
-                                <h2>Discover, Select
-                                    And Listen to Extraordinary Songs </h2>
+                                <h2><span className="gradient-text-bluegreen">Discover</span>, Select
+                                    And <span className="gradient-text-pink">Listen</span> to Extraordinary <span className="gradient-text-bluepink">Songs</span> </h2>
                                 <p>Digital music platforms that gives you access to thousand of songs from creators all over the world.</p>
                                 <div className="banner-btns d-flex flex-wrap justify-content-center">
                                     <Link href="/indextwo">
@@ -40,47 +40,7 @@ const Banner = () => {
                     </div>
                 </div>
             </div>
-        <div className="banner_shape">
-            <div className="banner_shape__1">
-                <div className="row g-3 mb-3">
-                    {
-                        BannerImageListOne.map((item) =>(
-                            <BannerSingle data={item} key={item.id} />
-                        ))
-                    }
-                    
-                </div>
-
-                <div className="row g-3">
-                    {
-                        BannerImageListTwo.map((item) =>(
-                            <BannerSingle data={item} key={item.id} />
-                        ))
-                    }
-                    
-                </div>
-                
-            </div>
-            <div className="banner_shape__2">
-            <div className="row g-3 mb-3">
-                    {
-                        BannerImageListOne.map((item) =>(
-                            <BannerSingle data={item} key={item.id} />
-                        ))
-                    }
-                    
-                </div>
-
-                <div className="row g-3">
-                    {
-                        BannerImageListTwo.map((item) =>(
-                            <BannerSingle data={item} key={item.id} />
-                        ))
-                    }
-                    
-                </div>
-            </div>
-        </div>
+        
         </section>
         </div>
     )

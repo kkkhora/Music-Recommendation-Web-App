0b6a6aabba204bed21853f7ecc4ac54e956e1dbc
fondatsang <97462055+fondatsang@users.noreply.github.com>
2022-04-26 18:32:35 +0800

    logo

--- Spotify Music Collection (Client)/components/Header.js
+++ Spotify Music Collection (Client)/components/Header.js
@@ -38,7 +38,7 @@ const Header = () => {
                     <div className="header__logo">
                         <Link href="/">
                         <a>
-                            <img src="assets/images/logo/logo.gif" alt="logo"></img>
+                            <img src="assets/images/logo/logo_clean_small.png" alt="logo"></img>
                         </a>
                         </Link>
                     </div>

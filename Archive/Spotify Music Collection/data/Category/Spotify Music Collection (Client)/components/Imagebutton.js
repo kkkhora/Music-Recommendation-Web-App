9c1da50cf344f967ee88407b603bdb20595048b3
kkkhora <76552190+kkkhora@users.noreply.github.com>
2022-07-26 16:35:05 +0800

    Delete Archive directory

--- Archive/Spotify Music Collection/data/Category/Spotify Music Collection (Client)/components/Imagebutton.js
+++
@@ -1,9 +0,0 @@
-import ImageButton from 'react-image-button';
-
-const App = () => {
-  return (
-    <ImageButton img="assets/images/logo/spotify_logo2.png" >
-      <button primary> Image-button </button>
-    </ImageButton>
-  )
-}
